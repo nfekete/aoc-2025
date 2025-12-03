463cbd69aa256b898f5ffe1bceb174716696d619
Nándor Előd Fekete <nandor.fekete@gmail.com>
2025-12-03 09:53:39 +0100

    Day 3 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2025/day03/Day03.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2025/day03/Day03.kt
@@ -2,16 +2,19 @@ package me.nfekete.adventofcode.y2025.day03
 
 import me.nfekete.adventofcode.y2024.common.classpathFile
 
-private fun String.maxJoltage() =
-    map { it.digitToInt() }.let { digits ->
-        digits.dropLast(1).max().let { msd ->
-            msd * 10 + digits.dropWhile { it < msd }.drop(1).max()
-        }
+private fun String.maxJoltage(joltageDigits: Int) =
+    map { it.digitToInt() }.let { allDigits ->
+        (joltageDigits-1 downTo 0).fold(allDigits to emptyList<Int>()) { (digits, outputDigits), exponent ->
+            digits.dropLast(exponent).withIndex().maxBy { it.value }.let {
+                digits.drop(it.index + 1) to outputDigits + it.value
+            }
+        }.second.joinToString(separator = "").toLong()
     }
 
 private fun main() {
     val input = classpathFile("day03/input.txt")
         .readLines()
 
-    input.sumOf { it.maxJoltage() }.also { println("Part1: $it") }
+    input.sumOf { it.maxJoltage( 2) }.also { println("Part1: $it") }
+    input.sumOf { it.maxJoltage(12) }.also { println("Part2: $it") }
 }
