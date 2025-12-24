76912634680ee337702e5a10ad75118dc918296b
Nándor Előd Fekete <nandor.fekete@gmail.com>
2025-12-24 22:59:12 +0200

    Day 11 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2025/day11/Day11.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2025/day11/Day11.kt
@@ -2,16 +2,27 @@ package me.nfekete.adventofcode.y2025.day11
 
 import me.nfekete.adventofcode.y2025.common.classpathFile
 import me.nfekete.adventofcode.y2025.common.map2
+import me.nfekete.adventofcode.y2025.common.memoized
 import me.nfekete.adventofcode.y2025.common.splitByDelimiter
 
-private fun Map<String, List<String>>.part1(): Long {
+private class Problem(val map: Map<String, List<String>>) {
     fun recurse(from: String, to: String): Long =
         if (from == to)
             1
         else
-            getValue(from).sumOf { recurse(it, to) }
+            map[from]?.sumOf { recurseM(it, to) } ?: 0
 
-    return recurse("you", "out")
+    val recurseM = ::recurse.memoized()
+    fun part1() = recurseM("you", "out")
+    fun part2(): Long {
+        val fftFirst = recurseM("fft", "dac") > 0
+        val (c1, c2) = if (fftFirst) "fft" to "dac" else "dac" to "fft"
+        val leg1 = recurseM("svr", c1)
+        val leg2 = recurseM(c1, c2)
+        val leg3 = recurseM(c2, "out")
+        return leg1 * leg2 * leg3
+    }
+
 }
 
 private fun main() {
@@ -21,5 +32,8 @@ private fun main() {
             line.splitByDelimiter(": ").map2 { it.split(' ') }
         }
 
-    input.part1().also { println("Part1: $it") }
+    Problem(input).let { problem ->
+        problem.part1().also { println("Part1: $it") }
+        problem.part2().also { println("Part2: $it") }
+    }
 }
